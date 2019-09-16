eb8eb7a5386cd1d03daf4c6c102227693e18d0cd
Arun Ramakani <arun@hubun.ae>
2019-09-16 23:16:39 +0400

    update

--- client.py
+++ client.py
@@ -15,19 +15,23 @@ def client_streaming(stub):
 
     def request_messages():
         for i in range(100000):
-            request = datastream_pb2.Request(
-                request_data=("called by Python client, message:%d" % i))
+            request = datastream_pb2.Request(user=(datastream_pb2.User(id=i, name=("name-%d" % i), message=("Welcome-%d" % i))))
             yield request
 
     response = stub.ClientStreaming(request_messages(), metadata=[('network-international', '643524tr^#sXT')])
     print("resp from server with status=%s" % (response.result))
-    print("--------------Call ClientStreamingMethod Over---------------")
+    
+    if response.result == 0:
+        print("--------------Call ClientStreamingMethod Over With Success---------------")
+    elif a == b:
+        print("--------------Call ClientStreamingMethod Over With Failure---------------")
 
 
 def main():
     chan_ops = [('grpc.default_compression_algorithm', CompressionAlgorithm.gzip),
             ('grpc.grpc.default_compression_level', CompressionLevel.high)]
-    with grpc.insecure_channel(SERVER_ADDRESS,chan_ops) as channel:
+ 
+    with grpc.secure_channel(SERVER_ADDRESS, grpc.ssl_channel_credentials(open('server.crt', 'rb').read())) as channel:
         stub = datastream_pb2_grpc.GRPCDataStreamStub(channel)
         client_streaming(stub)
 
